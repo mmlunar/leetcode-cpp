4e54bc8d4f9070fe2b3250f6d93b4fa53c1930bd
Mohammad Mosiur Rahman Lunar <mmlunar@hotmail.com>
2024-09-28 18:00:21 -0700

    Fixing smal issues.

--- src/CustomAssert.cpp
+++ src/CustomAssert.cpp
@@ -5,8 +5,8 @@ void CustomAssert::run(int x, int y)
     if (x != y) 
     {
         std::cout << "Expected: " << x << ", Actual: " << y << std::endl;
-        assert(x == y);
     }
+    assert(x == y);
 }
 
 void CustomAssert::run(int x, int y, std::string message) 
@@ -14,6 +14,6 @@ void CustomAssert::run(int x, int y, std::string message)
     if (x != y) 
     {
         std::cout << message << " Expected: " << x << ", Actual: " << y << std::endl;
-        assert(x == y);
     }
+    assert(x == y);
 }
